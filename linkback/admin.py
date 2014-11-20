402f29c4f29a5d3721b8e4dfb5e1f00e494d90d7
Corey Farwell <coreyf@rwell.org>
2014-11-19 21:39:40 -0500

    Change to work on raw ids

--- linkback/admin.py
+++ linkback/admin.py
@@ -1,10 +1,7 @@
-from .widgets import SelectWithLinkWidget
+from .widgets import ForeignKeyRawIdWithLinkWidget
 
 
 class LinkBackMixin(object):
     def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
-        try:
-            kwargs['widget'] = SelectWithLinkWidget(db_field.rel.to)
-        except TypeError:  # django 1.4+
-            kwargs['widget'] = SelectWithLinkWidget(db_field.rel.to, self.admin_site)
+        kwargs['widget'] = ForeignKeyRawIdWithLinkWidget(db_field.rel, self.admin_site)
         return db_field.formfield(**kwargs)
--- linkback/widgets.py
+++ linkback/widgets.py
@@ -1,44 +1,109 @@
 from django import forms
+from django.core.urlresolvers import reverse
 from django.utils.safestring import mark_safe
+import six
 
-#An interest value of this would be x.object
-LOOKUP_IMAGE = lambda x: '<img src="/static/admin/img/selector-search.gif" style="cursor: pointer; margin-left: 5px" width="16" height="16" alt="Lookup">'
+from django.utils.html import escape
+from django.utils.translation import ugettext as _
+from django.utils.text import Truncator
+
+from .templatetags import static
 
 
-class ModelLinkWidget(forms.Widget):
-    def __init__(self, obj, attrs=None):
-        self.object = obj
-        super(ModelLinkWidget, self).__init__(attrs)
+def url_params_from_lookup_dict(lookups):
+    """
+    Converts the type of lookups specified in a ForeignKey limit_choices_to
+    attribute to a dictionary of query parameters
+    """
+    params = {}
+    if lookups and hasattr(lookups, 'items'):
+        items = []
+        for k, v in lookups.items():
+            if callable(v):
+                v = v()
+            if isinstance(v, (tuple, list)):
+                v = ','.join([str(x) for x in v])
+            elif isinstance(v, bool):
+                # See django.db.fields.BooleanField.get_prep_lookup
+                v = ('0', '1')[v]
+            else:
+                v = six.text_type(v)
+            items.append((k, v))
+        params.update(dict(items))
+    return params
+
+
+class ForeignKeyRawIdWidget(forms.TextInput):
+    """
+    A Widget for displaying ForeignKeys in the "raw_id" interface rather than
+    in a <select> box.
+    """
+    def __init__(self, rel, admin_site, attrs=None, using=None):
+        self.rel = rel
+        self.admin_site = admin_site
+        self.db = using
+        super(ForeignKeyRawIdWidget, self).__init__(attrs)
 
     def render(self, name, value, attrs=None):
-        if self.object.pk:
-            return mark_safe(
-                u'<a target="_blank" href="../../../%s/%s/%s/">%s</a>' %\
-                      (
-                       self.object._meta.app_label,
-                       self.object._meta.object_name.lower(),
-                       self.object.pk, LOOKUP_IMAGE(self)
-                       )
-            )
-        else:
-            return mark_safe(u'')
+        rel_to = self.rel.to
+        if attrs is None:
+            attrs = {}
+        extra = []
+        if rel_to in self.admin_site._registry:
+            # The related object is registered with the same AdminSite
+            related_url = reverse('admin:%s_%s_changelist' %
+                                    (rel_to._meta.app_label,
+                                    rel_to._meta.model_name),
+                                    current_app=self.admin_site.name)
+
+            params = self.url_parameters()
+            if params:
+                url = '?' + '&amp;'.join(['%s=%s' % (k, v) for k, v in params.items()])
+            else:
+                url = ''
+            if "class" not in attrs:
+                attrs['class'] = 'vForeignKeyRawIdAdminField' # The JavaScript code looks for this hook.
+            # TODO: "lookup_id_" is hard-coded here. This should instead use
+            # the correct API to determine the ID dynamically.
+            extra.append('<a href="%s%s" class="related-lookup" id="lookup_id_%s" onclick="return showRelatedObjectLookupPopup(this);"> '
+                            % (related_url, url, name))
+            extra.append('<img src="%s" width="16" height="16" alt="%s" /></a>'
+                            % (static('admin/img/selector-search.gif'), _('Lookup')))
+        output = [super(ForeignKeyRawIdWidget, self).render(name, value, attrs)] + extra
+        if value:
+            output.append(self.label_for_value(value))
+        return mark_safe(''.join(output))
+
+    def base_url_parameters(self):
+        return url_params_from_lookup_dict(self.rel.limit_choices_to)
+
+    def url_parameters(self):
+        from django.contrib.admin.views.main import TO_FIELD_VAR
+        params = self.base_url_parameters()
+        params.update({TO_FIELD_VAR: self.rel.get_related_field().name})
+        return params
+
+    def label_for_value(self, value):
+        key = self.rel.get_related_field().name
+        try:
+            obj = self.rel.to._default_manager.using(self.db).get(**{key: value})
+            return '&nbsp;<strong>%s</strong>' % escape(Truncator(obj).words(14, truncate='...'))
+        except (ValueError, self.rel.to.DoesNotExist):
+            return ''
 
 
-class SelectWithLinkWidget(forms.widgets.Select):
-    def __init__(self, klass, attrs=None, choices=(), *args, **kwargs):
-        self.klass = klass
-        super(SelectWithLinkWidget, self).__init__(attrs, choices, *args, **kwargs)
-
+class ForeignKeyRawIdWithLinkWidget(ForeignKeyRawIdWidget):
     def render(self, *args, **kwargs):
-        result = super(SelectWithLinkWidget, self).render(*args, **kwargs)
+        result = super(ForeignKeyRawIdWithLinkWidget, self).render(
+            *args, **kwargs)
         name, value = args
         if not value:
             return result
-        return mark_safe(result +
-                u'<a target="_blank" href="../../../%s/%s/%s/">%s</a>' %\
-                      (
-                       self.klass._meta.app_label,
-                       self.klass._meta.object_name.lower(),
-                       value, LOOKUP_IMAGE(self)
-                       )
+        return mark_safe(
+            result +
+            u' - <a href="../../../%s/%s/%s/">Admin Link</a>' % (
+                self.rel.to._meta.app_label,
+                self.rel.to._meta.object_name.lower(),
+                value,
+            )
         )
