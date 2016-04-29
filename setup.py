9700eb5256fb77368eea8318b8f578b2aebadc2c
Chris Streeter <chris@chrisstreeter.com>
2016-04-29 06:29:34 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-rawid-linkback",
-    version="0.1-ec1-70860fd",
+    version="0.1-ec2-040cbef",
     author='Lincoln Loop: Nicolas Lara',
     author_email='info@lincolnloop.com',
     description=("An admin widget to show a link back to the original objects in foreign keys."),
